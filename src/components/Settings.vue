6ade3511001b064f14f4352ded2278c844c6ad47
Matt Grosso <matt.grosso@framebridge.com>
2023-06-29 22:42:22 -0400

    Fixes tag removal methods

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -7,12 +7,12 @@
       <div class="p-1 border-white border">
         <p class="m-0 text-center fs-5">:: {{$store.state.databaseTopKey}} ::</p>
       </div>
-      <div class="tags movie-tags p-3 border border-white mt-3">
+      <div class="tags viewing-tags p-3 border border-white mt-3">
         <ul class="col-12 p-2 border border-white">
           <li class="tag mb-2 col-6 dflex align-items-center" v-for="(tag, index) in viewingTags" :key="index">
             <span class="badge col-12 rounded-pill text-bg-light" @click="showRemoveButton($event)">
               {{ tag.title }}
-              <span class="remove-button" @click.prevent="removeTag(index)">
+              <span class="remove-button" @click.prevent="removeViewingTag(index)">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-circle-fill text-light" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                 </svg>
@@ -29,12 +29,12 @@
           </div>
         </div>
       </div>
-      <div class="tags viewing-tags p-3 border border-white mt-3">
+      <div class="tags movie-tags p-3 border border-white mt-3">
         <ul class="col-12 p-2 border border-white">
           <li class="tag mb-2 col-6 dflex align-items-center" v-for="(tag, index) in movieTags" :key="index">
             <span class="badge col-12 rounded-pill text-bg-light" @click="showRemoveButton($event)">
               {{ tag.title }}
-              <span class="remove-button" @click.prevent="removeTag(index)">
+              <span class="remove-button" @click.prevent="removeMovieTag(index)">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-circle-fill text-light" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                 </svg>
@@ -273,10 +273,17 @@ export default {
 
       event.target.classList.toggle('show-remove-button');
     },
-    async removeTag (tagIndex) {
+    async removeViewingTag (tagIndex) {
       await set(ref(
         getDatabase(),
-        `${this.databaseTopKey}/settings/tags/${tagIndex}`),
+        `${this.databaseTopKey}/settings/tags/viewing-tags/${tagIndex}`),
+      null
+      );
+    },
+    async removeMovieTag (tagIndex) {
+      await set(ref(
+        getDatabase(),
+        `${this.databaseTopKey}/settings/tags/movie-tags/${tagIndex}`),
       null
       );
     },
