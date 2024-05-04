a89918c6320e89c007a0b6628b5b3fd502341998
Matt Grosso <matt.grosso@framebridge.com>
2024-05-04 19:12:15 -0400

    Runs linter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -870,8 +870,8 @@ export default {
     }
   },
   methods: {
-    toTitleCase(str) {
-      return str.replace(/\w\S*/g, function(txt) {
+    toTitleCase (str) {
+      return str.replace(/\w\S*/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       });
     },
